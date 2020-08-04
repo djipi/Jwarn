044bfbe4e18c1fc9ff17b8e12687cce9c72a64f2
Jean-Paul Mari <djipi.mari@gmail.com>
2020-08-03 21:09:28 -0400

    First commit

---
+++ VS2019/portab.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <windows.h>
+
+#define EXTERN extern
+#define GLOBAL
+#define MLOCAL static
+#define REG register
+//#define TRUE true
+//#define FALSE false
+#define NULLPTR NULL
+
+//typedef char BYTE;
+//typedef short int WORD;
+//typedef long LONG;
+//typedef void VOID;
